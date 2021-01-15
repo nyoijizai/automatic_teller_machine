1554d3e37f87c9e471cfbbb260d15480e215df69
liudaming <liudaming@huke.tv>
2021-01-15 16:13:15 +0800

    「优化」修改配置项

--- src/api/utils/base.js
+++ src/api/utils/base.js
@@ -1,9 +1,8 @@
 const route = {
- IC: 'http://127.0.0.1:8787/v4',
- ID: 'http://127.0.0.1:24010/ZKIDROnline',
- serve: 'https://m.tzu.edu.cn/api',
- test: 'http://192.168.3.194:30090/api',
- // test: 'http://portal-tzu-edu-cn-s.webvpn.tzu.edu.cn:8118/api',
+ IC: '',
+ ID: '',
+ serve: '',
+ test: '',
 };
 const server =
  process.env.NODE_ENV === 'development' ? route.test : process.env.VUE_APP_BASE;
